54bf4a15e7ac925c9664370baccdd124252945ef
Gabriel <130802677+gab44h@users.noreply.github.com>
2024-05-13 15:14:57 -0300

    Delete Conexao.java

--- Conexao.java
+++
@@ -1,9 +0,0 @@
-package Códigos;
-import java.sql.*;
-public class Conexao {
-    
-    public Connection getConnection()throws SQLException{
-        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/APS-Database", "postgres", "120305");
-        return conexao;
-    }
-}
